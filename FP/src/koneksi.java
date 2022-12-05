190b99b3aab6cc834a262f104dd132796ddade1f
Abhinaya173 <114990549+Abhinaya173@users.noreply.github.com>
2022-12-05 22:27:46 +0700

    Add files via upload

---
+++ FP/src/koneksi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+/**
+ *
+ * @author ABHINAYA
+ */
+public class koneksi {
+    Connection koneksi=null;
+    public static Connection koneksiDB()
+    {
+        
+        return null;
+        
+    }
+}
